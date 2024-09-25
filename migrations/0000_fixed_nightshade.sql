df7471e539f3789b3abd8b6440a6c690e4fd896d
Misha Sokol <m.sokol.misha@gmail.com>
2024-09-25 14:21:46 +0300

    Datapase setup

--- migrations/0000_fixed_nightshade.sql
+++ migrations/0000_fixed_nightshade.sql
@@ -23,6 +23,18 @@ CREATE TABLE IF NOT EXISTS "folders" (
 	"workspaces_id" uuid
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "workspaces" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone,
+	"workspace_owner" uuid NOT NULL,
+	"title" text NOT NULL,
+	" icon_id" text NOT NULL,
+	"data" text NOT NULL,
+	"in_trash" text,
+	"logo" text,
+	"banner_url" text
+);
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "files" ADD CONSTRAINT "files_workspaces_id_workspaces_id_fk" FOREIGN KEY ("workspaces_id") REFERENCES "workspaces"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import {
+  boolean,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
+import { subscriptionStatus, prices } from './../../../migrations/schema';
 
 export const workspaces = pgTable('workspaces', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
@@ -50,4 +60,45 @@ export const files = pgTable('files', {
   folderId: uuid('folder_id').references(() => folders.id, {
     onDelete: 'cascade',
   }),
+});
+
+export const subscriptions = pgTable('subscriptions', {
+  id: text('id').primaryKey().notNull(),
+  userId: uuid('user_id').notNull(),
+  status: subscriptionStatus('status'),
+  metadata: jsonb('metadata'),
+  priceId: text('price_id').references(() => prices.id),
+  quantity: integer('quantity'),
+  cancelAtPeriodEnd: boolean('cancel_at_period_end'),
+  created: timestamp('created', { withTimezone: true, mode: 'string' }).default(
+    sql`new()`
+  ),
+  currentPeriodStart: timestamp('current_period_start', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
+  currentPeriodEnd: timestamp('current_period_end', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
+  endedAt: timestamp('ended_at', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
+  cancelAt: timestamp('cancel_at', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
+  canceledAt: timestamp('canceled_at', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
+  trialStart: timestamp('trial_start', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
+  trialEnd: timestamp('trial_end', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`new()`),
 });
