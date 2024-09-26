9e92f773fa3343287beff337618c57fd78668f2c
Misha Sokol <m.sokol.misha@gmail.com>
2024-09-26 15:24:20 +0300

    Add bannerlogo

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,8 @@
 import TitleSection from '@/components/landing-page/title-section';
+import { Button } from '@/components/ui/button';
 import React from 'react';
+import Banner from '../../../public/appBanner.png';
+import Image from 'next/image';
 
 type Props = {};
 
@@ -12,11 +15,17 @@ const Homepage = () => {
           title="All-In-One Collaboration and Productivity Platform"
         />
         <div className="mt-6 rounded-xl bg-white bg-gradient-to-r from-primary to-brand-primaryBlue p-[2px] sm:w-[300px]">
-
-          <Button>
-            
+          <Button
+            variant="btn-secondary"
+            className="w-full rounded-[10px] bg-background p-6 text-2xl"
+          >
+            Get Cypress Free
           </Button>
         </div>
+      </div>
+      <div className="relative ml-[-50px] mt-[-40px] flex w-[750px] items-center justify-center sm:ml-0 sm:w-full md:mt-[-90px]">
+        <Image src={Banner} alt="Application Banner" />
+        <div className="absolute bottom-0 left-0 right-0 top-[50%] z-10 bg-gradient-to-t dark:from-background"></div>
       </div>
     </section>
   );
